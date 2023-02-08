1f3b7bbe2e0160998bc236870d9eb514a70ca3cb
Jesse-James PRINCE A <37332216+pajjaecat@users.noreply.github.com>
2023-02-08 16:49:23 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -36,6 +36,25 @@ extensions = [
     "sphinx.ext.napoleon",
 ]
 
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+
+
 intersphinx_mapping = {
     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
     "python": ("https://docs.python.org/3/", None),
